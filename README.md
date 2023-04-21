b2d4032cc3970f5f2c1d38417d1a96f99ce84c7e
stancom <crusader6344@gmail.com>
2023-04-21 10:57:57 +1000

    the first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Server Status Indicator
+
+Using bash shell ping to server and show time, ip and status
+
+# Notes
+Output constantly clears and refreshes (single line), as opposed to continuously printing output (multiple lines).
+
+# Preview
+
+![alt](https://i.imgur.com/4vgARZF.png)
---
+++ server_status.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+host=$1
+
+if [ -z $host ]; then
+    echo "Usage: `basename $0` [HOST]"
+    exit 1
+fi
+
+while :; do
+    tput clear
+    result=`ping -W 1 -c 1 $host | grep 'bytes from '`
+    if [ $? -gt 0 ]; then
+        echo -e "`date +'%Y/%m/%d %H:%M:%S'` - host $host is \033[0;31mOFFLINE\033[0m"
+    else
+         echo -e "`date +'%Y/%m/%d %H:%M:%S'` - host $host is \033[0;32mONLINE\033[0m -`echo $result | cut -d ':' -f 2`"
+        sleep 1 # avoid ping rain
+fi
+done
